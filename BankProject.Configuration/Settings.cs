c7bc36abf8647a79b1b9a000be1cf0c3bbd98e64
tibi-ului <tiberiu.ului@ausy.com>
2023-01-06 12:26:47 +0200

    update Settings.cs file

--- BankProject.Configuration/Settings.cs
+++ BankProject.Configuration/Settings.cs
@@ -11,10 +11,10 @@ namespace BankProject.Configuration
         /// <summary>
         /// Customer number starts from 1001;incremented by 1
         /// </summary>
-        public static long BaseCustomerNo { get; set; } = 10000;
-        public static long BaseAccountNo { get; set; } = 100;
-        public static long BaseFundsTransferNo { get; set; } = 10;
-        public static long BaseFundsTransferStatementNo { get; set; } = 10;
-        public static long BaseAccountStatementNo { get; set; } = 0;
+        public static long BaseCustomerNo { get; set; } = 1;
+        public static long BaseAccountNo { get; set; } = 1;
+        public static long BaseFundsTransferNo { get; set; } = 1;
+        public static long BaseFundsTransferStatementNo { get; set; } = 1;
+        public static long BaseAccountStatementNo { get; set; } = 1;
     }
 }
